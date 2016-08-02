3fdf43aaf0750415fc18e056a13c377470146651
Tomer <Tomer>
2016-08-02 18:49:07 +0300

    Updated get_data script and fixed bug that ignored some matches, and also handling more-than-one-day-matches better now

--- get_data.py
+++ get_data.py
@@ -15,59 +15,33 @@ class MatchObj(object):
         self.app_param = app_param
         self.end_time = end_time
 
+    def __repr__(self):
+        return "<MatchObj match: %r, app: %r, end time: %r" % (self.match, self.app_param, self.end_time)
+
 
 def main():
-    matchs_data = []
+    matches_data = []
     time_of_earliest_matches_to_get = (datetime.datetime.now() - datetime.timedelta(days=90)).date()
     upcoming_matches = Match.objects.filter(date__gte=time_of_earliest_matches_to_get).order_by("date")
 
-    # Choose arbitrary dates in the past
-    last_date = datetime.date(2000, 1, 1)
-    current_date = datetime.date(2000, 1, 1)
+    last_date = upcoming_matches.first().date
+    current_date = last_date
     matches_on_last_date = {}
     matches_on_current_date = {}
 
-#    for match in upcoming_matches:
-#        # If we are already 2 days after the last saved date, switch it to the
-#        # current date (which should now be "yesterday")
-#        if match.date - last_date > datetime.timedelta(days=1):
-#            last_date = current_date
-#            matches_on_last_date = matches_on_current_date
-#
-#            current_date = match.date
-#            matches_on_current_date = {}
-#
-#        app_param = None
-#        url = ""
-#        try:
-#            # TODO: Don't choose first app params but use some other way to decide which app params to take
-#            app_param = AppParamsTeam.objects.filter(team=match.home_team).first().app_params
-#            url = app_param.meta_data["og_url"]
-#        except:
-#            print("Failed to find url for match id %s" % (match.id))
-#            pass
-#
-#        if match.title in matches_on_last_date:
-#            match_obj = matches_on_last_date[match.title]
-#            match_obj["end"] = current_date + datetime.timedelta(days=1)
-#            matches_on_current_date[match.title] = match_obj
-#        else:
-#            match_obj = {
-#                #"match_id": match.id,
-#                "title": "%s: %s" % (app_param.app_name, match.title) if app_param is not None else match.title,
-#                "start": match.date, #.strftime("%Y-%m-%d"),
-#                "end": 0, # match.strftime("%Y-%m-%d"),
-#                "url": app_param.meta_data["og_url"] if app_param is not None else "",
-#            }
-#            matchs_data.append(match_obj)
-#            matches_on_current_date[match.title] = match_obj
-
     for match in upcoming_matches:
-        # If we are already 2 days after the last saved date, switch it to the
-        # current date (which should now be "yesterday")
-        if match.date - last_date > datetime.timedelta(days=1):
+        if match.date - current_date >= datetime.timedelta(days=1):
             last_date = current_date
-            matches_on_last_date = matches_on_current_date
+            if match.date - current_date >= datetime.timedelta(days=2):
+                # If we are already 2 days after the last saved date, start
+                # fresh (there are no previous matches that will continue to
+                # the current date)
+                matches_on_last_date = {}
+            else:
+                # We are 1 day later than the previous match's date, so if a
+                # match from the previous day will repeat itself we will unify
+                # them
+                matches_on_last_date = matches_on_current_date
 
             current_date = match.date
             matches_on_current_date = {}
@@ -80,8 +54,9 @@ def main():
             print("Failed to find app param for match id %s" % (match.id))
 
         if match.title in matches_on_last_date:
+            # This match is a continuation of a match from the previous day
             match_obj = matches_on_last_date[match.title]
-            match_obj.end = current_date + datetime.timedelta(days=1)
+            match_obj.end_time = current_date + datetime.timedelta(days=1)
             matches_on_current_date[match.title] = match_obj
         else:
             match_obj = MatchObj(
@@ -89,12 +64,12 @@ def main():
                 app_param=app_param,
                 end_time=None
             )
-            matchs_data.append(match_obj)
+            matches_data.append(match_obj)
             matches_on_current_date[match.title] = match_obj
 
     # Transform match objects to event format
     events_data = []
-    for match_obj in matchs_data:
+    for match_obj in matches_data:
         title = match_obj.match.title
         url = ""
         if match_obj.app_param:
